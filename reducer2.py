5971ebfa418b32779f97af8bc6437eb6ae027589
Kaemri <klaidas.karalevicius@gmail.com>
2025-03-22 23:44:08 +0200

    fixed reducer to count values of missing keys

--- reducer2.py
+++ reducer2.py
@@ -8,6 +8,11 @@ def reduce_function():
         'siuntu_skaicius': 0,
         'klientu_skaicius': 0
     })
+    # store missing key combinations
+    missing_keys_data = defaultdict(lambda: {
+        "siuntu_skaicius": 0,
+        "klientu_skaicius": 0
+    })
 
     none_counts = {
         'geografine_zona': 0,
@@ -31,14 +36,23 @@ def reduce_function():
             none_counts['siuntu_skaicius'] += 1
         if klientu_skaicius == 'none':
             none_counts['klientu_skaicius'] += 1
-        # skip calculation if there is missing values
-        if 'none' in (siuntu_skaicius, klientu_skaicius):
-            continue
-        # summ values for each key pair
-        area_data[(geografine_zona, sav_diena)]['siuntu_skaicius'] += int(siuntu_skaicius)
-        area_data[(geografine_zona, sav_diena)]['klientu_skaicius'] += int(klientu_skaicius)
-    # output result
+        # convert values into integers
+        siuntu_skaicius = int(siuntu_skaicius) if siuntu_skaicius != "none" else 0
+        klientu_skaicius = int(klientu_skaicius) if klientu_skaicius != "none" else 0
+        # write data normally if both keys exist
+        if geografine_zona != "none" and sav_diena != "none":
+            area_data[(geografine_zona, sav_diena)]["siuntu_skaicius"] += siuntu_skaicius
+            area_data[(geografine_zona, sav_diena)]["klientu_skaicius"] += klientu_skaicius
+        else:
+            # if any key is missing, write data collected from missing key instances
+            missing_keys_data[(geografine_zona, sav_diena)]["siuntu_skaicius"] += siuntu_skaicius
+            missing_keys_data[(geografine_zona, sav_diena)]["klientu_skaicius"] += klientu_skaicius
+
+    # output result of full data
     for (geografine_zona, sav_diena), data in area_data.items():
+        print(f"{geografine_zona},{sav_diena}\t{data['siuntu_skaicius']},{data['klientu_skaicius']}")
+    # output result of data with missing key
+    for (geografine_zona, sav_diena), data in missing_keys_data.items():
         print(f"{geografine_zona},{sav_diena}\t{data['siuntu_skaicius']},{data['klientu_skaicius']}")
     # output missing values
     for field, count in none_counts.items():
